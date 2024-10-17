618ec6b79396bf2674cacdec676a2d64faf943a2
ashiq2003758 <ashiq2003758@gmail.com>
2024-10-17 20:42:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+document.getElementById("welcome-btn").addEventListener("click", function() {
+    alert("Hello, welcome to my page!");
+});
+
+
+document.getElementById("profile-pic").addEventListener("mouseover", function() {
+    document.body.style.backgroundColor = "#e0e0e0";
+});
+
+document.getElementById("profile-pic").addEventListener("mouseout", function() {
+    document.body.style.backgroundColor = "#f0f0f0";
+});
